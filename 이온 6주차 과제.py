a1fa6f09ac2233e1083fc83d8c7f92d3225b8ccb
Songhaejin <101044238+Songhaejin@users.noreply.github.com>
2022-04-30 18:06:18 +0900

    Update 이온 6주차 과제.py

--- 이온 6주차 과제.py
+++ 이온 6주차 과제.py
@@ -1,7 +1,7 @@
 a = int(input("조각 수 : "))
 
-arr= [1,2]
+arr= [1,1]
 
-for i in range(2, a) :
-	arr.append(arr[i-1] + arr[i-2])
-print(arr[N-1])
+for i in range(2, a+1) :
+	arr.append(arr[i-2] + arr[i-1])
+print(arr[a])
